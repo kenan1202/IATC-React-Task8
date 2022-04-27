6be9598459eda66e4ce023694034caed06f25b79
kenan1202 <kenan.tanriverdi.1@gmail.com>
2022-04-27 18:02:13 +0400

    React Project Updated

--- src/components/Currencies.js
+++ src/components/Currencies.js
@@ -7,17 +7,17 @@ const CryptoCurrencies = ({ data }) => {
       <h1> Search a currency</h1>
       <div>
         <input
-          placeholder="Search" 
-          className="search-bttn"
+          placeholder="Search Currency..." 
+          className="searchBtn"
           onChange={(e) => setSearch(e.target.value)}
         />
       </div>
 
-      <div className="crypto-wrapper">
+      <div className="cryptoWrap">
         {data
           .filter((element) => element.name.toLowerCase().includes(search))
-          .map((item) => (
-            <div className="crypto-item">
+          .map((item, id) => (
+            <div className="cryptoItem" key = {id}>
               <img src={item.image} alt = "Item Image"/>
               <h3>{item.name}</h3>
               <div>{item.symbol}</div>
